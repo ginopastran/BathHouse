6f41cb063c7b33b1859bf17c273f362c49675b89
Ciro Metral <metralciro@gmail.com>
2024-01-19 10:40:54 -0300

    Refactor popover trigger in ProfileForm component

--- app/otro/page.tsx
+++ app/otro/page.tsx
@@ -816,7 +816,10 @@ export default function ProfileForm() {
     <div>
 
     <Popover>
-    <PopoverTrigger>Mirar el presupuesto</PopoverTrigger>
+      <div className="flex justify-center pb-6">
+
+    <PopoverTrigger className="  rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 w-[50%]">Mirar el presupuesto</PopoverTrigger>
+      </div>
     <PopoverContent className="h-[800px] w-screen">
       <Popoverdata />
     </PopoverContent>
