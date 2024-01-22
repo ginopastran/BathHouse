6081d698b8fb3f6ecab224480fd7a1d01fd4ad4a
Ciro Metral <metralciro@gmail.com>
2024-01-22 15:06:33 -0300

    Remove unused code and add new link etapa-presupuesto-2

--- app/page.tsx
+++ app/page.tsx
@@ -2,65 +2,23 @@
 
 import React, { useState } from "react";
 import axios from "axios";
-import { Button } from "@/components/ui/button";
 
-const provinciasArgentina = [
-  "Buenos Aires",
-  "Catamarca",
-  "Chaco",
-  "Chubut",
-  "Córdoba",
-  "Corrientes",
-  "Entre Ríos",
-  "Formosa",
-  "Jujuy",
-  "La Pampa",
-  "La Rioja",
-  "Mendoza",
-  "Misiones",
-  "Neuquén",
-  "Río Negro",
-  "Salta",
-  "San Juan",
-  "San Luis",
-  "Santa Cruz",
-  "Santa Fe",
-  "Santiago del Estero",
-  "Tierra del Fuego",
-  "Tucumán",
-];
 
 const FormularioPage: React.FC = () => {
-  const [perimetro, setPerimetro] = useState<number>();
-  const [ubicacionObra, setUbicacionObra] = useState<string>("Buenos Aires"); // Valor predeterminado
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    try {
-      await axios.post("/api/actualizarExcel", { perimetro, ubicacionObra });
-
-      const response = await axios.get("/api/actualizarExcel", {
-        responseType: "blob",
-      });
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", "BATHOUSE-Enero-2024.xlsx");
-      document.body.appendChild(link);
-      link.click();
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
-    <div className=" w-full h-screen  p-4">
+    <div className="  flex    p-4 gap-5">
       <a
         href="/otro"
         className=" bg-white px-3 py-2 text-black rounded-lg text-sm font-medium"
       >
         Presupuesto 1
+      </a>
+      <a
+        href="/etapa-2"
+        className=" bg-white px-3 py-2 text-black rounded-lg text-sm font-medium"
+      >
+        Presupuesto 2
       </a>
     </div>
   );
