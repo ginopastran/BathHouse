1e7994c39c7b6baee097000f66163ae64a0dd473
ginopastran <ginopastran@gmail.com>
2024-02-02 23:58:10 -0300

    Navbar Hydration Bug fixed

--- components/shared/navbar.tsx
+++ components/shared/navbar.tsx
@@ -12,21 +12,96 @@ import {
   NavbarMenu,
   NavbarMenuItem,
 } from "@nextui-org/react";
-import { getSession, useSession } from "next-auth/react";
-import { signOut } from "@/auth";
+import { useSession } from "next-auth/react";
 import { logout } from "@/actions/logout";
 import { Session } from "next-auth";
+import { LoginButton } from "../auth/login-button";
 
 interface NavBarProps {
   session: Session | null;
 }
 
-export default function MainNavbar({ session }: NavBarProps) {
+export default function MainNavbar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const onClick = () => {
     logout();
   };
+
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <Navbar onMenuOpenChange={setIsMenuOpen}>
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
+          />
+          <NavbarBrand>
+            <Link href="/">
+              <p className="font-bold text-inherit text-2xl">Bathouse</p>
+            </Link>
+          </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+            <Link href="/otro" aria-current="page">
+              Presupuesto 1
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="/etapa-2">
+              Presupuesto 2
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem className="hidden sm:flex gap-3"></NavbarItem>
+        </NavbarContent>
+        <NavbarMenu className="flex pt-8">
+          <NavbarMenuItem className="flex flex-col gap-y-6">
+            <Link href="/otro" className=" text-lg">
+              Presupuesto 1
+            </Link>
+            <Link href="/etapa-2" className=" text-lg">
+              Presupuesto 2
+            </Link>
+            {session && (
+              <NavbarItem>
+                <Button
+                  color="primary"
+                  onClick={onClick}
+                  variant="flat"
+                  className=" bg-sky-600"
+                >
+                  Log Out
+                </Button>
+              </NavbarItem>
+            )}
+            {!session && (
+              <div className="flex gap-x-4">
+                <NavbarItem className="flex">
+                  <LoginButton mode="modal" asChild>
+                    <Link className=" cursor-pointer">Login</Link>
+                  </LoginButton>
+                </NavbarItem>
+                <NavbarItem>
+                  <Button
+                    as={Link}
+                    href="/auth/register"
+                    className=" bg-sky-600"
+                  >
+                    Sign Up
+                  </Button>
+                </NavbarItem>
+              </div>
+            )}
+          </NavbarMenuItem>
+        </NavbarMenu>
+      </Navbar>
+    );
+  }
 
   return (
     <Navbar onMenuOpenChange={setIsMenuOpen}>
@@ -54,7 +129,7 @@ export default function MainNavbar({ session }: NavBarProps) {
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex gap-3">
+        <NavbarItem className="hidden sm:flex gap-3">
           {session && (
             <NavbarItem>
               <Button
@@ -68,27 +143,61 @@ export default function MainNavbar({ session }: NavBarProps) {
             </NavbarItem>
           )}
           {!session && (
-            <NavbarItem className="hidden lg:flex">
-              <Link href="/auth/login">Login</Link>
+            <NavbarItem className=" lg:flex">
+              <LoginButton mode="modal" asChild>
+                <Button variant="bordered" className=" border-sky-600">
+                  Login
+                </Button>
+              </LoginButton>
             </NavbarItem>
           )}
           {!session && (
             <NavbarItem>
-              <Button as={Link} href="/auth/register" className=" bg-sky-600">
+              <Button
+                as={Link}
+                href="/auth/register"
+                className=" bg-sky-600 text-sm"
+              >
                 Sign Up
               </Button>
             </NavbarItem>
           )}
         </NavbarItem>
       </NavbarContent>
-      <NavbarMenu className="flex items-center pt-8">
-        <NavbarMenuItem className="flex flex-col gap-y-3">
+      <NavbarMenu className="flex pt-8">
+        <NavbarMenuItem className="flex flex-col gap-y-6">
           <Link href="/otro" className=" text-lg">
             Presupuesto 1
           </Link>
           <Link href="/etapa-2" className=" text-lg">
             Presupuesto 2
           </Link>
+          {session && (
+            <NavbarItem>
+              <Button
+                color="primary"
+                onClick={onClick}
+                variant="flat"
+                className=" bg-sky-600"
+              >
+                Log Out
+              </Button>
+            </NavbarItem>
+          )}
+          {!session && (
+            <div className="flex gap-x-4">
+              <NavbarItem className="flex">
+                <LoginButton mode="modal" asChild>
+                  <Link className=" cursor-pointer">Login</Link>
+                </LoginButton>
+              </NavbarItem>
+              <NavbarItem>
+                <Button as={Link} href="/auth/register" className=" bg-sky-600">
+                  Sign Up
+                </Button>
+              </NavbarItem>
+            </div>
+          )}
         </NavbarMenuItem>
       </NavbarMenu>
     </Navbar>
