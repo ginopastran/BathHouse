b9b86e63072061c49c76ea2aec0f534fa85060bf
ginopastran <ginopastran@gmail.com>
2024-02-13 11:11:52 -0300

    Sign In button

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "../app/style";
 import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => (
-  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col pb-8`}>
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
       <div className="flex-[1] flex flex-col justify-start mr-10">
         <img
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,13 +21,13 @@ const Hero = () => {
         <p className="md:text-2xl text-xl font-bold text-gray-500">
           Deja tu proyecto en nuestras manos, te brindamos un hogar.
         </p>
-        {/* <div className="pt-6">
+        <div className="pt-6">
           <LoginButton mode="modal" asChild>
             <Button variant="secondary" size="lg">
               Sign In
             </Button>
           </LoginButton>
-        </div> */}
+        </div>
       </div>
     </div>
   );
