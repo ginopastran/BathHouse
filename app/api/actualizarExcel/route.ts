d6061e51a6ac81df3fc3d8f297f85ffd9e80afcf
ginopastran <ginopastran@gmail.com>
2024-02-16 00:54:42 -0300

    AWS region changed to us-east

--- app/api/actualizarExcel/route.ts
+++ app/api/actualizarExcel/route.ts
@@ -27,7 +27,7 @@ export async function OPTIONS() {
 AWS.config.update({
     accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
-    region: "sa-east-1",
+    region: "us-east-2",
 });
 
 const s3 = new AWS.S3();
