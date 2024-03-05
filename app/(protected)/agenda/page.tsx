ca31696a7db2be617949a1ac516a08a1198d69cb
Ciro Metral <metralciro@gmail.com>
2024-03-05 18:28:22 -0300

    Actualización de la función de búsqueda para incluir filtros de fecha y categoría

---
+++ app/(protected)/agenda/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="absolute z-40 h-full w-full">
+      <iframe
+        src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ3EUi5SkvbWM7ymFYfOdQ9qMEAUaq74AM423xPiV7T9yir3e_A-moOGvEP589cfTVDCFULRKPh-?gv=true"
+        className="border-none "
+        width="100%"
+        height="100%"
+        
+      ></iframe>
+    </div>
+  );
+};
+
+export default page;
