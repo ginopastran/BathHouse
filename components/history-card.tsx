89283f3e456d6b75b97ad4510f27d9158caa36dd
ginopastran <ginopastran@gmail.com>
2024-03-08 10:11:51 -0300

    changed colors in History Card

--- components/history-card.tsx
+++ components/history-card.tsx
@@ -32,10 +32,10 @@ function HistoryCard({ requestNumber, date, jsonData }: HistoryCardProps) {
   const [selectedJson, setSelectedJson] = useState<JsonData | null>(null);
 
   return (
-    <div className="flex items-center justify-between space-x-4  w-full px-3 py-2 rounded-2xl border-2 border-blue-950">
+    <div className="flex items-center justify-between space-x-4  w-full px-3 py-2 rounded-2xl border-2 border-orange-800">
       <div className="space-y-1 flex items-center justify-center">
         <div className="w-14">
-          <div className="relative w-4 h-4 rounded-full border border-gray-200 bg-white dark:border-slate-700 dark:bg-blue-950" />
+          <div className="relative w-4 h-4 rounded-full border border-gray-200 bg-white dark:border-orange-700 dark:bg-orange-800" />
         </div>
         <div className="flex flex-col">
           <h3 className="font-bold">{requestNumber}</h3>
@@ -44,12 +44,18 @@ function HistoryCard({ requestNumber, date, jsonData }: HistoryCardProps) {
       </div>
       <div className="flex items-center justify-end gap-2 flex-col sm:flex-row">
         <BudgetHistoryButton xlsxName={requestNumber}>
-          <Button variant="bordered" className="border-blue-950 w-full text-sm">
+          <Button
+            variant="bordered"
+            className="border-orange-800 w-full text-sm"
+          >
             Presupuesto
           </Button>
         </BudgetHistoryButton>
         <EditJsonButton jsonData={jsonData}>
-          <Button variant="bordered" className="border-blue-950 w-full text-sm">
+          <Button
+            variant="bordered"
+            className="border-orange-800 w-full text-sm"
+          >
             Editar
           </Button>
         </EditJsonButton>
