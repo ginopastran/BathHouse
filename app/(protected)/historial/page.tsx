ff91c9b3388ebfdafeabf59dcd4056715b016330
ginopastran <ginopastran@gmail.com>
2024-03-15 02:11:56 -0300

    navbar for historypage fixed

--- app/(protected)/historial/page.tsx
+++ app/(protected)/historial/page.tsx
@@ -71,26 +71,28 @@ function HistoryPage() {
   }
 
   return (
-    <div className="flex flex-col h-screen dark relative pt-8 z-30">
+    <>
       <NewCompNavbar title="Historial" />
-      <main className="flex-1 overflow-auto p-4 text-white">
-        <div className="container max-w-3xl space-y-8">
-          <div className="flex flex-col items-center space-y-2 text-center"></div>
-          <div className="grid gap-8">
-            {data.map((item, index) => (
-              <div className="flex items-center space-x-4" key={index}>
-                <HistoryCard
-                  requestNumber={item["nombre-obra"]}
-                  date={formatDateString(item["fecha"])}
-                  jsonData={item}
-                />
-              </div>
-            ))}
+      <div className="flex flex-col h-screen dark relative pt-8 z-30">
+        <main className="flex-1 overflow-auto p-4 text-white">
+          <div className="container max-w-3xl space-y-8">
+            <div className="flex flex-col items-center space-y-2 text-center"></div>
+            <div className="grid gap-8">
+              {data.map((item, index) => (
+                <div className="flex items-center space-x-4" key={index}>
+                  <HistoryCard
+                    requestNumber={item["nombre-obra"]}
+                    date={formatDateString(item["fecha"])}
+                    jsonData={item}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      </main>
-      <footer className="flex items-center h-14 px-4 border-t border-gray-100 dark:border-gray-800"></footer>
-    </div>
+        </main>
+        <footer className="flex items-center h-14 px-4 border-t border-gray-100 dark:border-gray-800"></footer>
+      </div>
+    </>
   );
 }
 
